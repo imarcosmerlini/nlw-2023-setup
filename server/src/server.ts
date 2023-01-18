7ebbe871c153cc612f011bdc769ef1ae1a905e91
Marcos Merlini <merlinimarcos@gmail.com>
2023-01-18 17:39:06 -0300

    Created base rotes for backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,13 @@
 import FastiFy from 'fastify'
-import {PrismaClient} from '@prisma/client'
 import cors from '@fastify/cors'
+import {appRoutes} from "./routes"
 
 const app = FastiFy()
-const prisma = new PrismaClient()
 
 app.register(cors, {
 	origin: ['http://localhost:3000']
 })
-
-app.get('/habits', async () => {
-	return prisma.habit.findMany({});
-})
+app.register(appRoutes)
 
 app.listen({
 	port: 3333,
